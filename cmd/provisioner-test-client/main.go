ec5f398c67613651cb8d8f2df2ad4a77e9f9857c
Lorenzo Stoakes <lstoakes@gmail.com>
2016-06-10 12:17:30 +0100

    Use client test helper function.

--- cmd/provisioner-test-client/main.go
+++ cmd/provisioner-test-client/main.go
@@ -16,6 +16,14 @@ func usage() {
 	os.Exit(1)
 }
 
+func tryGet(client *SocketClient, path string) {
+	if str, err := client.Get(path); err == nil {
+		log.Printf("GET  /%s: '%s'\n", path, str)
+	} else {
+		log.Printf("GET  /%s: ERROR: %s\n", path, err)
+	}
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		usage()
@@ -24,11 +32,7 @@ func main() {
 	path := os.Args[1]
 	client := NewSocketClient(path)
 
-	if str, err := client.Get("provision"); err == nil {
-		log.Printf("GET  /provision: '%s'\n", str)
-	} else {
-		log.Printf("GET  /provision: ERROR: %s\n", err)
-	}
+	tryGet(client, "provision")
 
 	json := `{"foo": "abc", "bar": 3}`
 	if str, err := client.PostJsonString("provision", json); err == nil {
