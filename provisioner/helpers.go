b0040b9e0ba5678a08625a8d8ab5739000028a47
Lorenzo Stoakes <lstoakes@gmail.com>
2016-06-10 11:24:32 +0100

    Add first attempt at atomic write logic.

--- provisioner/helpers.go
+++ provisioner/helpers.go
@@ -26,3 +26,29 @@ func readerToString(r io.Reader) (ret string, err error) {
 
 	return
 }
+
+func atomicWrite(path, content string) error {
+	// Try to be atomic - write output to temporary file, sync it, rename it
+	// to the target file.
+
+	// "" is the dir - defaults to system temp dir, "provisioner" is prefix.
+	if tmpFile, err := ioutil.TempFile("", "provisioner"); err != nil {
+		return err
+	} else {
+		name := tmpFile.Name()
+
+		// We ignore the error so removing the now renamed file isn't an
+		// issue. In error cases we clean up.
+		defer os.Remove(name)
+
+		if _, err := tmpFile.Write([]byte(content)); err != nil {
+			return err
+		} else if err := tmpFile.Sync(); err != nil {
+			return err
+		} else if err := tmpFile.Close(); err != nil {
+			return err
+		}
+
+		return os.Rename(name, path)
+	}
+}
