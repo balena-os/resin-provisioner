9c7567e8a8a1ce70a540bb7870dc8f42935b9b20
Lorenzo Stoakes <lstoakes@gmail.com>
2016-06-10 11:56:32 +0100

    Move API functionality into api.go.

--- provisioner/api.go
+++ provisioner/api.go
@@ -1,1 +1,30 @@
 package provisioner
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+)
+
+type Api struct {
+	ConfigPath string
+	listener   net.Listener
+	server     *http.Server
+}
+
+func New(configPath string) *Api {
+	ret := &Api{ConfigPath: configPath}
+	ret.initSocket()
+
+	return ret
+}
+
+func (a *Api) getProvision() (string, error) {
+	if conf, err := a.readConfig(); err != nil {
+		return "", fmt.Errorf("Cannot read config: %s", err)
+	} else if str, err := stringify(conf); err != nil {
+		return "", fmt.Errorf("Cannot stringfy config: %s", err)
+	} else {
+		return str, nil
+	}
+}
