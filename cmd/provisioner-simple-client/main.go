3653ef4140e9070299b43d2bb2e0b78ff291a035
Lorenzo Stoakes <lstoakes@gmail.com>
2016-06-13 18:15:31 +0100

    Add the ability to read the current provisioned status.
    The simple client will output the current provisioned status when invoked with
    only the config path, e.g.:
    
    $ ./provisioner-simple-client /mnt/conf/config.json
    provisioning
    $

--- cmd/provisioner-simple-client/main.go
+++ cmd/provisioner-simple-client/main.go
@@ -14,25 +14,38 @@ func init() {
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "usage: %s [config path] [user id] [application id] [api key]\n",
+	fmt.Fprintf(os.Stderr, "usage:     query: %s [config path]\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "usage: provision: %s [config path] [user id] [app id] [api key]\n",
 		os.Args[0])
 	os.Exit(1)
 }
 
 func main() {
-	if len(os.Args) < 5 {
+	if len(os.Args) < 5 && len(os.Args) != 2 {
 		usage()
 	}
 
 	configPath := os.Args[1]
+	api := provisioner.New(configPath)
+
+	// Simply output the provision state.
+	if len(os.Args) == 2 {
+		if state, err := api.State(); err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
+			os.Exit(1)
+		} else {
+			fmt.Printf("%s\n", state)
+		}
+
+		return
+	}
+
 	userId := os.Args[2]
 	appId := os.Args[3]
 	apiKey := os.Args[4]
 
 	opts := &provisioner.ProvisionOpts{
 		UserId: userId, ApplicationId: appId, ApiKey: apiKey}
-
-	api := provisioner.New(configPath)
 
 	if err := api.Provision(opts); err != nil {
 		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
