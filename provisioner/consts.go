c630a0c199ae0c40b4deb4b99cc0f7d4722a6a65
Lorenzo Stoakes <lstoakes@gmail.com>
2016-06-13 15:41:17 +0100

    go fmt.

--- provisioner/consts.go
+++ provisioner/consts.go
@@ -1,6 +1,6 @@
 package provisioner
 
 const (
-	SUPERVISOR_PATH = "/lib/systemd/system/resin-supervisor.service"
+	SUPERVISOR_PATH         = "/lib/systemd/system/resin-supervisor.service"
 	UPDATE_RESIN_TIMER_PATH = "/lib/systemd/system/update-resin-supervisor.timer"
 )
